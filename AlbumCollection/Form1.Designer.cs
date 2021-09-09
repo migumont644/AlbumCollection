0136a57ba5984ccd8083520bfaf703665fba265e
P1 <P1@TK_021>
2021-09-09 11:21:57 -0400

    Add project files.

---
+++ AlbumCollection/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace AlbumCollection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.albumInfoBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.sortLabel = new System.Windows.Forms.Label();
+            this.orderLabel = new System.Windows.Forms.Label();
+            this.originalLabel = new System.Windows.Forms.Label();
+            this.sortedLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-4, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter album information";
+            // 
+            // albumInfoBox
+            // 
+            this.albumInfoBox.Location = new System.Drawing.Point(234, 13);
+            this.albumInfoBox.Name = "albumInfoBox";
+            this.albumInfoBox.Size = new System.Drawing.Size(320, 22);
+            this.albumInfoBox.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(581, 13);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(84, 35);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeButton.Location = new System.Drawing.Point(671, 13);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(101, 35);
+            this.removeButton.TabIndex = 3;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // sortLabel
+            // 
+            this.sortLabel.BackColor = System.Drawing.Color.White;
+            this.sortLabel.Location = new System.Drawing.Point(455, 97);
+            this.sortLabel.Name = "sortLabel";
+            this.sortLabel.Size = new System.Drawing.Size(316, 327);
+            this.sortLabel.TabIndex = 5;
+            // 
+            // orderLabel
+            // 
+            this.orderLabel.BackColor = System.Drawing.Color.White;
+            this.orderLabel.Location = new System.Drawing.Point(29, 97);
+            this.orderLabel.Name = "orderLabel";
+            this.orderLabel.Size = new System.Drawing.Size(316, 327);
+            this.orderLabel.TabIndex = 6;
+            // 
+            // originalLabel
+            // 
+            this.originalLabel.AutoSize = true;
+            this.originalLabel.BackColor = System.Drawing.Color.White;
+            this.originalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.originalLabel.Location = new System.Drawing.Point(29, 97);
+            this.originalLabel.Name = "originalLabel";
+            this.originalLabel.Size = new System.Drawing.Size(104, 16);
+            this.originalLabel.TabIndex = 7;
+            this.originalLabel.Text = "Original Order";
+            // 
+            // sortedLabel
+            // 
+            this.sortedLabel.AutoSize = true;
+            this.sortedLabel.BackColor = System.Drawing.Color.White;
+            this.sortedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortedLabel.Location = new System.Drawing.Point(455, 97);
+            this.sortedLabel.Name = "sortedLabel";
+            this.sortedLabel.Size = new System.Drawing.Size(96, 16);
+            this.sortedLabel.TabIndex = 8;
+            this.sortedLabel.Text = "Sorted Order";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sortedLabel);
+            this.Controls.Add(this.originalLabel);
+            this.Controls.Add(this.orderLabel);
+            this.Controls.Add(this.sortLabel);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.albumInfoBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Album Collection ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox albumInfoBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Label sortLabel;
+        private System.Windows.Forms.Label orderLabel;
+        private System.Windows.Forms.Label originalLabel;
+        private System.Windows.Forms.Label sortedLabel;
+    }
+}
+
---
+++ AlbumCollection/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlbumCollection
+{
+    public partial class Form1 : Form
+    {
+        List<string> orderList = new List<string>();
+        List<string> sortedList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            orderList.Add(albumInfoBox.Text);
+            sortedList.Add(albumInfoBox.Text);
+
+            orderLabel.Text = "";
+            for (int i = 0; i < orderList.Count(); i++)
+
+            {
+                orderLabel.Text += "\n" + orderList[i];
+                        
+            }
+
+            sortedList.Sort();
+            sortLabel.Text = "";            
+            for (int i = 0; i < sortedList.Count(); i++)
+            {             
+                sortLabel.Text += "\n" + sortedList[i];
+            }
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
